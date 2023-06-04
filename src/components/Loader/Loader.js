92d8388292be4e9586b8bec9967cd7024869acd7
Plamena Mihaylova <plamena.pavlova.mihaylova@gmail.com>
2023-06-04 11:37:34 +0300

    Fetch any series name and add some additional functionality.

---
+++ src/components/Loader/Loader.js
@@ -0,0 +1,11 @@
+import loader from "../../assets/loader.gif";
+
+function Loader() {
+  return (
+    <div>
+      <img style={{ width: 75 }} src={loader} alt="loader" />
+    </div>
+  );
+}
+
+export default Loader;
--- src/components/containers/series/Series.js
+++ src/components/containers/series/Series.js
@@ -1,19 +1,37 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import fetch from "cross-fetch";
 import SeriesList from "../../SeriesList/SeriesList";
+import Loader from "../../Loader/Loader";
 
 function Series() {
   const [series, setSeries] = useState([]);
+  const [seriesName, setSeriesName] = useState("");
+  const [isFetching, setIsFetching] = useState(false);
 
-  useEffect(() => {
-    fetch("http://api.tvmaze.com/search/shows?q=Vikings")
+  const onSeriesInputChange = (e) => {
+    setSeriesName(e.target.value);
+    setIsFetching(true);
+    fetch(`http://api.tvmaze.com/search/shows?q=${e.target.value}`)
       .then((response) => response.json())
-      .then((json) => setSeries(json));
-  });
+      .then((json) => {
+        setSeries(json);
+        setIsFetching(false);
+      });
+  };
+
   return (
     <div>
-      The length of series array - {series.length}
-      <SeriesList list={series} />
+      <div>
+        <input value={seriesName} type="text" onChange={onSeriesInputChange} />
+      </div>
+      {!isFetching && series.length === 0 && seriesName.trim() === "" && (
+        <p>Please, enter series name to the input.</p>
+      )}
+      {!isFetching && series.length === 0 && seriesName.trim() !== "" && (
+        <p>No TV series with this name have been found.</p>
+      )}
+      {isFetching && <Loader />}
+      {!isFetching && <SeriesList list={series} />}
     </div>
   );
 }
